9a40b7778ef9bc4d4a98b8f4b8ce23fd769a87a6
Deepak2000 <dj1676106@gmail.com>
2023-05-19 18:55:23 +0530

    Added button to refresh status

--- src/doctor/Components/ThankYou/ThankYou.jsx
+++ src/doctor/Components/ThankYou/ThankYou.jsx
@@ -1,36 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Logo from '../../assets/Logo-carepay.webp'
 import Lens from '../../assets/lens.png'
 
 import './thankYou.scss'
 import axios from 'axios';
 import { env } from '../../environment';
-
-let phoneNo = localStorage.getItem('phoneNumber');
-let email = localStorage.getItem('email');
-
-
-let doctorId = localStorage.getItem('doctorId');
+import { useNavigate } from 'react-router-dom';
 
 const ThankYou = () =>{
-    useEffect(()=>{
-        doctorId = localStorage.getItem('doctorId');
-        phoneNo = localStorage.getItem('phoneNumber');
-        email = localStorage.getItem('email');
 
-        if(doctorId){
-            console.log(doctorId)
-            axios.get(env.api_Url+"generateQRCode?doctorId=" + doctorId)
+    const [email, setEmail] = useState(localStorage.getItem('emailId'));
+    const [doctorId, setDoctorId] = useState(localStorage.getItem('doctorId'));
+    const [phoneNo, setPhoneNo] = useState(localStorage.getItem('phoneNumber'));
+
+    const navigate = useNavigate();
+
+    // useEffect(()=>{
+    //     if(doctorId){
+    //         // console.log(doctorId)
+    //         // axios.get(env.api_Url+"generateQRCode?doctorId=" + doctorId)
+    //         // .then(response =>{
+    //         //     console.log(response)
+    //         // })
+
+            
+    //     }
+    // }, [doctorId])
+
+    function checkVerificationStatus(e){
+        if(!! doctorId){
+            e.target.innerText = "Checking...";
+            axios.get(env.api_Url + "getDoctorVerificationStatus?doctorId=" + doctorId)
             .then(response =>{
-                console.log(response)
+                console.log(response);
+                if(response.data.status === 200){
+                    if(response.data.data === "VERIFIED"){
+                        navigate('/doctor/dashboard');
+                    }else{
+                        e.target.innerText = "Please try after some time";
+                        setTimeout(() => {
+                            e.target.innerText = "Refresh status";
+                        }, 1500);
+                    }
+                }else{
+                    e.target.innerText = "Refresh status";
+                }
+            }).catch(error =>{
+                console.log(error);
+                e.target.innerText = "Refresh status";
             })
         }
-        
-        axios.get(env.api_Url + "sendUnderReviewMail?doctorId=" + doctorId)
-        .then(response =>{
-            console.log(response)
-        })
-    }, [])
+    }
+
     return(
         <main id='thankYou'>
             <div className='head'><img id='logo' src={Logo} alt="Carepay's Logo" /></div>
@@ -39,6 +60,7 @@ const ThankYou = () =>{
             <p className='line1'>Your application is under review,<br/>we will inform you once verified.</p>
             <p className='line2'>This typically takes around 30 mins...</p>
             <div className="msg">You will be notified on your<br/>registered contact number <strong style={{whiteSpace: "nowrap"}}>+91 {phoneNo}</strong><br/> and email-id <strong style={{whiteSpace: "nowrap"}}>{email}</strong> once your application is verified.</div>
+            <button onClick={(e)=>checkVerificationStatus(e)} className="submit">Refresh status</button>
         </main>
     )
 }
