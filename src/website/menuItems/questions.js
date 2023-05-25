ef70740045a6a271b18c1f73a58c73d296de048e
Deepak2000 <dj1676106@gmail.com>
2023-05-25 17:23:13 +0530

    Corrected all questions and answers

--- src/website/menuItems/questions.js
+++ src/website/menuItems/questions.js
@@ -1,18 +1,18 @@
 const questions = [
   {
     id: 1,
-    title: "How Do I Know If My Healthcare Provider Accepts carepay?",
+    title: "How do I know if my healthcare provider accepts CarePay?",
     ans: "You can easily find us at a participating clinic, as our QR code is displayed or you can ask your healthcare provider.",
   },
   {
     id: 2,
-    title: "What Is The Eligibility Criteria To Use CarePay?",
-    ans: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, modi!",
+    title: "What is the eligibility criteria to use CarePay?",
+    ans: " To be eligible for CarePay, you must be at least 18 years old, have a valid ID, and meet our credit assessment criteria.",
   },
   {
     id: 3,
-    title: "Can I You CarePay For Emergency Treatments?",
-    ans: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, accusamus dignissimos ea itaque nostrum adipisci.",
+    title: "Can I use CarePay for emergency treatments?",
+    ans: "Currently, CarePay is designed for elective treatments only. We're working on expanding our services to cover emergency treatments in the future.",
   },
 ];
 
