61d7d7023379c6126db0641e94bbf31e715cc505
Deepak2000 <dj1676106@gmail.com>
2023-11-08 19:11:50 +0530

    JSON animation test

--- src/patient/components/ICICI flow/SearchingOffers.jsx
+++ src/patient/components/ICICI flow/SearchingOffers.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
 import SearchImg from '../../assets/GIFs/searching.png'
 import Header from '../Header/Header'
+import lottie from "lottie-web";
+import animationData from '../../assets/JSON animations/Comp 1.json'
 const SearchingOffers = () =>{
 
+
+    useEffect(() => {
+        lottie.loadAnimation({
+          container: document.querySelector("#searchAnimation"),
+          animationData: animationData,
+          renderer: "canvas"
+        });
+      }, []);
     return(
         <main style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
             <Header progressbarDisplay="none" />
-            <img src={SearchImg} alt="searching" style={{border:"1px solid black", width:"60%", marginTop:"30%"}} />
+            {/* <img src={SearchImg} alt="searching" style={{border:"1px solid black", width:"60%", marginTop:"30%"}} /> */}
+            <div id="searchAnimation"></div>
             <p style={{marginTop:"1rem"}}>Searching best offers for you...</p>
         </main>
     )
