d5c68e3e8791194b0c9ee4c16d4720d0e38e2af3
Deepak2000 <dj1676106@gmail.com>
2024-04-23 14:44:33 +0530

    TnC updated

--- src/website/pages/WebHomepage.jsx
+++ src/website/pages/WebHomepage.jsx
@@ -642,8 +642,9 @@ export function Footer(){
                             <h5>Quick links</h5>
                             <Link to={"/about"}>About Us</Link>
                             <Link to={"/privacyPolicy"}>Privacy Policy</Link>
-                            <Link to={"/patient"}>For Patients</Link>
-                            <Link to={"/doctor"}>For Doctors</Link>
+                            <Link to={"/TermsAndConditions"}>Terms and conditions</Link>
+                            {/* <Link to={"/patient"}>For Patients</Link>
+                            <Link to={"/doctor"}>For Doctors</Link> */}
                             <Link to={"/lendingPartners"}>Our lending partners</Link>
                         </div>
                         <div className="contact-us">
