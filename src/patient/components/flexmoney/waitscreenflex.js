6304a63c59bcdb8828a1ed686c9849b032a1ac69
Abhinav Rana <48202760+ranaabhinav726@users.noreply.github.com>
2024-06-13 14:38:00 +0530

    chn

--- src/patient/components/flexmoney/waitscreenflex.js
+++ src/patient/components/flexmoney/waitscreenflex.js
@@ -4,7 +4,9 @@ import Loadinggif from '../../../utils/loader/Loading 3.gif';
 import { createOrderApiFm } from './actioncreator';
 
 const CheckoutComponent = () => {
-    const [orderTokenData,setOrderTokenData]=useState('')
+    const [orderTokenData, setOrderTokenData] = useState('')
+    const [loaderState, setloaderState] = useState(true)
+
     window.onICMerchantSDKReady = () => {
         window.ICMerchantSDK.init({
             merchantKey: '91838b89-1634-4d44-866e-722995f4c5f0',
@@ -28,38 +30,40 @@ const CheckoutComponent = () => {
     };
 
     useEffect(() => {
-        createOrderApiFm(localStorage.getItem('userId'),callback=>{
+        createOrderApiFm(localStorage.getItem('userId'), callback => {
             console.log(callback)
             setOrderTokenData(callback.data)
-             setTimeout(() => {
-            handleCheckout()
-        }, 10000);
+            setloaderState(false)
+            // setTimeout(() => {
+            //     handleCheckout()
+            // }, 10000);
         })
         const script = document.createElement('script');
         script.async = true;
         script.src = 'https://iccdn.in/web-merchant-sdk-uat/v-1.0/static/instaCred-merchant-sdk.js';
         document.body.appendChild(script);
-       
+
         return () => {
             document.body.removeChild(script);
         };
-       
+
     }, []);
-    
+
 
     return (
 
         <main className='congrats'>
             <Header progressbarDisplay="none" />
-             
-            <img src={Loadinggif}/>
-            <p className='text-center'>connecting with your bank...</p>
+
+            {loaderState ? <img src={Loadinggif} /> : ""}
+            {loaderState ?  <p className='text-center'>connecting with your bank...</p>:""}
             {/* <button onClick={handleCheckout}>Checkout with InstaCred</button> */}
             <div id="instaCredWidget"></div>
-            <button className='' style={{marginTop:'20px',padding:'15px',color:'#504c9a',background:'#ecebfd',border:'none',borderRadius:'5px',fontSize:'14px',fontWeight:'700'}}>Refresh</button>
+           {orderTokenData !==''? <button className='' onClick={handleCheckout} style={{ marginTop: '20px', padding: '15px', color: '#504c9a', background: '#ecebfd', border: 'none', borderRadius: '5px', fontSize: '14px', fontWeight: '700' }}>Continue</button>:""}
+            {/* <button onClick={handleCheckout}>Continue </button> */}
             <p style={{ textAlign: 'center', fontSize: '14px', marginTop: '10px', marginBottom: '10px', marginTop: '120px' }}>Need help? Reach out to us.-</p>
 
-            <a style={{ color: '#000',textDecoration:'none',width:'100%' }} href={"tel:+91 806 948 9655"}>  <button className="submit" style={{ background: '#ECEBFF', color: "#514C9F", marginTop: '-6px' }}>Contact Support</button></a>
+            <a style={{ color: '#000', textDecoration: 'none', width: '100%' }} href={"tel:+91 806 948 9655"}>  <button className="submit" style={{ background: '#ECEBFF', color: "#514C9F", marginTop: '-6px' }}>Contact Support</button></a>
         </main>
     );
 };
